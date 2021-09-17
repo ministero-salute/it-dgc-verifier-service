d5ac354559d4b508f3af78ee0b8f4a458793663d
AndreaPerquoti <ap92@hotmail.it>
2021-09-17 19:52:50 +0200

    (fix) fix after merge

--- src/main/java/it/interop/dgc/verifier/service/DRLSRV.java
+++ src/main/java/it/interop/dgc/verifier/service/DRLSRV.java
@@ -3,13 +3,11 @@ package it.interop.dgc.verifier.service;
 import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import it.interop.dgc.verifier.entity.DeltaETY;
 import it.interop.dgc.verifier.entity.SnapshotETY;
 import it.interop.dgc.verifier.entity.dto.DRLDTO;
-import it.interop.dgc.verifier.exceptions.DgcaBusinessRulesResponseException;
 import it.interop.dgc.verifier.repository.DeltaRepository;
 import it.interop.dgc.verifier.repository.SnapshotRepository;
 
@@ -17,16 +15,15 @@ import it.interop.dgc.verifier.repository.SnapshotRepository;
  * 
  * @author CPIERASC
  *
- *	Servizio gestione CRL - Certificate Revocation List.
  */
-@Service 
+@Service
 public class DRLSRV implements Serializable {
 
 	/**
      * 
      */
     private static final long serialVersionUID = -6489834121456735245L;
-   
+  
     
 	/**
 	 * Pass repo.
@@ -39,36 +36,20 @@ public class DRLSRV implements Serializable {
 	 */
 	@Autowired
 	private DeltaRepository deltaRepo;
-	 
-  
-	 
-    public DRLDTO getDRL(Long version, boolean isIspettiva) {
-        SnapshotETY crl = null;
-        Long lastSnap = snapRepo.getLastVersion();
-        DeltaETY delta = deltaRepo.getByVersions(version, lastSnap);
-        
-    
-        if(lastSnap==null) {
-            throw new DgcaBusinessRulesResponseException(HttpStatus.BAD_REQUEST,"0x004","Nessuno snapshot trovato sul db",null,null);  
-        }
-        
-        if(version!=null && version>lastSnap) {
-            throw new DgcaBusinessRulesResponseException(HttpStatus.BAD_REQUEST,"0x004","Versione maggiore rispetto all'ultima snapshot disponibile",null,null);  
-        }
-         
-        if(lastSnap.equals(version)) {
-            crl = snapRepo.getSnapWithoutUCVI(version);
-        } else if (delta == null) {
-            crl = snapRepo.getLastVersionWithContent();
-        }
-        
-        if(crl==null && delta==null) {
-            throw new DgcaBusinessRulesResponseException(HttpStatus.BAD_REQUEST,"0x004","Nessun record snap o delta trovato, controllare i dati passati",null,null);  
-        }
-        
-        return new DRLDTO(crl, delta);
-    }
-    
+	
+	public DRLDTO getDRL(Long version, boolean isIspettiva) {
+		SnapshotETY crl = null;
+		Long lastSnap = snapRepo.getLastVersion();
+		DeltaETY delta = deltaRepo.getByVersions(version, lastSnap);
+		
+		if(!isIspettiva && lastSnap.equals(version)) {
+		    crl = snapRepo.getSnapWithoutUCVI(version);
+		} else if (version==null || (isIspettiva && delta == null)) {
+			crl = snapRepo.getLastVersionWithContent();
+		}
+		
+		return new DRLDTO(crl, delta);
+	}
 	
  
  
