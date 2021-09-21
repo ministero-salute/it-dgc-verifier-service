c122e5206f8760a128afee5c8d794ed204b9d9da
AndreaPerquoti <ap92@hotmail.it>
2021-09-21 19:27:02 +0200

    (fix) check mongo empty collection

--- src/main/java/it/interop/dgc/verifier/service/DRLSRV.java
+++ src/main/java/it/interop/dgc/verifier/service/DRLSRV.java
@@ -1,13 +1,17 @@
 package it.interop.dgc.verifier.service;
 
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
 import it.interop.dgc.verifier.entity.DeltaETY;
 import it.interop.dgc.verifier.entity.SnapshotETY;
 import it.interop.dgc.verifier.entity.dto.DRLDTO;
+import it.interop.dgc.verifier.exceptions.DgcaBusinessRulesResponseException;
 import it.interop.dgc.verifier.repository.DeltaRepository;
 import it.interop.dgc.verifier.repository.SnapshotRepository;
-import java.io.Serializable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
  *
@@ -38,13 +42,26 @@ public class DRLSRV implements Serializable {
         SnapshotETY crl = null;
         Long lastSnap = snapRepo.getLastVersion();
         DeltaETY delta = deltaRepo.getByVersions(version, lastSnap);
-
-        if (!isIspettiva && lastSnap.equals(version)) {
+        
+    
+        if(lastSnap==null) {
+            throw new DgcaBusinessRulesResponseException(HttpStatus.BAD_REQUEST,"0x004","Nessuno snapshot trovato sul db",null,null);  
+        }
+        
+        if(version!=null && version>lastSnap) {
+            throw new DgcaBusinessRulesResponseException(HttpStatus.BAD_REQUEST,"0x004","Versione maggiore rispetto all'ultima snapshot disponibile",null,null);  
+        }
+         
+        if(lastSnap.equals(version)) {
             crl = snapRepo.getSnapWithoutUCVI(version);
-        } else if (version == null || (isIspettiva && delta == null)) {
+        } else if (delta == null) {
             crl = snapRepo.getLastVersionWithContent();
         }
-
+        
+        if(crl==null && delta==null) {
+            throw new DgcaBusinessRulesResponseException(HttpStatus.BAD_REQUEST,"0x004","Nessun record snap o delta trovato, controllare i dati passati",null,null);  
+        }
+        
         return new DRLDTO(crl, delta);
     }
 }
