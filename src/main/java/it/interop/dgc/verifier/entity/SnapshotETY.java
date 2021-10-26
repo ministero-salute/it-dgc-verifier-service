af3fd9f89b3ad3e76040e000a43d95b2e0cb4921
VincenzoIngenito <Vincenzo.Ingenito@ibm.com>
2021-10-26 18:27:15 +0200

    Update SnapshotETY.java

--- src/main/java/it/interop/dgc/verifier/entity/SnapshotETY.java
+++ src/main/java/it/interop/dgc/verifier/entity/SnapshotETY.java
@@ -1,16 +1,17 @@
 package it.interop.dgc.verifier.entity;
 
 import java.util.Date;
-import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
@@ -32,12 +33,7 @@ public class SnapshotETY {
      */
     @Field(name = "version")
     private Long version;
-
-    /**
-     * revoked_ucvi.
-     */
-//    @Field(name = "revoked_ucvi")
-//    private List<String> revokedUcvi;
+ 
 
     /**
      * creation_date.
