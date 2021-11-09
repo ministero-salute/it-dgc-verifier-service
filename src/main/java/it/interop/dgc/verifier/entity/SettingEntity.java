21093a07e41d25eecd2b7092de15fe73dde4798f
VincenzoIngenito <Vincenzo.Ingenito@ibm.com>
2021-11-09 12:32:50 +0100

    Update SettingEntity.java

--- src/main/java/it/interop/dgc/verifier/entity/SettingEntity.java
+++ src/main/java/it/interop/dgc/verifier/entity/SettingEntity.java
@@ -22,7 +22,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
-@Document(collection = "setting")
+@Document(collection = "settings")
 @AllArgsConstructor
 @NoArgsConstructor
 public class SettingEntity {
